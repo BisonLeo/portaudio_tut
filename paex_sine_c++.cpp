0c3c71f2b121943c3584f9379e847635eb84062d
RebornPowerHorse <leo@cleptrum.com>
2021-05-27 22:41:14 +0800

    sine example does not need ringbuffer

--- paex_sine_c++.cpp
+++ paex_sine_c++.cpp
@@ -46,14 +46,14 @@
 #include "portaudio.h"
 
 #define NUM_SECONDS   (5)
-#define SAMPLE_RATE   (44100)
+#define SAMPLE_RATE   (48000)
 #define FRAMES_PER_BUFFER  (64)
 
 #ifndef M_PI
 #define M_PI  (3.14159265)
 #endif
 
-#define TABLE_SIZE   (200)
+#define TABLE_SIZE   (500)
 
 class Sine
 {
